74ede7461f970823c89cecb7f802eca5c2a93a4c
pfranczak <franczak.przemyslaw3@gmail.com>
2020-03-17 22:46:11 +0100

    solution for heron exercise

--- 02-JSCoreConcepts/heron.js
+++ 02-JSCoreConcepts/heron.js
@@ -1,11 +1,21 @@
 // boki trójkąta
-//a = 3;
-b = 4;
-c = 5;
-h = 7
+const a = 3;
+const b = 4;
+const c = 5;
+// h = 7;
 
 // Pole trókąta o bokach ..., ... oraz ... wynosi ... .
 
-let a = () => "aaaa";
+function heron (a, b, c) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+        return 0;
+    }
 
-console.log( a() );
+    const p  = 0.5 * (a + b + c);
+
+    return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
+let foo = () => `Pole trókąta o bokach ${a}, ${b} oraz ${c} wynosi ${heron(a, b, c)}.`;
+
+console.log( foo() );
