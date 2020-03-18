a4cf67d1b59c2201c0d27532e38a94d2f53d5750
pfranczak <franczak.przemyslaw3@gmail.com>
2020-03-18 17:13:03 +0100

    solution for loop exercise

---
+++ 02-JSCoreConcepts/loopfor.js
@@ -0,0 +1,8 @@
+function loopfor () {
+
+    for (let number = 2; number <= 14; number += 4) {
+        console.log(number)
+    }
+}
+
+loopfor();
---
+++ 02-JSCoreConcepts/loopwhile.js
@@ -0,0 +1,11 @@
+function loopwhile () {
+    let number = 2;
+
+    while (number <= 14) {
+        console.log(number);
+
+        number += 4;
+    }
+}
+
+loopwhile();
